1465c193ac8d28e08963c04bcd560199ac3ea7f1
Håkon Løvdal <kode@denkule.no>
2019-01-27 13:59:35 +0100

    Remove cpp files not distributed in gem
    Temoprary local change, not something intended to be pushed upstream.
    
    This to enable using the repository as a git submodule without it picking
    up the example code.
    
    git rm -r SampleProjects
    git rm -r spec/fake_library

--- SampleProjects/TestSomething/test-something.cpp
+++
@@ -1,5 +0,0 @@
-#include "test-something.h"
-int testSomething(void) {
-  millis();  // this line is only here to test that we're able to refer to the builtins
-  return 4;
-};
--- SampleProjects/TestSomething/test/library.cpp
+++
@@ -1,9 +0,0 @@
-#include <ArduinoUnitTests.h>
-#include "../test-something.h"
-
-unittest(library_tests_something)
-{
-  assertEqual(4, testSomething());
-}
-
-unittest_main()
