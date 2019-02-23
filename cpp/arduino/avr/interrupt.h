8ed35b0c488829e1b25d13304d3b9c25ae85b098
Håkon Løvdal <kode@denkule.no>
2019-02-23 23:08:55 +0100

    Update <avr/interrupt.h>

--- cpp/arduino/avr/interrupt.h
+++ cpp/arduino/avr/interrupt.h
@@ -6,6 +6,11 @@
 */
 #pragma once
 
+// Macros for disable/enable/return from interrupts
+#define cli()
+#define sei()
+#define reti()
+
 // Allows the production code to define an ISR method.
 // These definitions come from the original avr/interrupt.h file
 // https://www.nongnu.org/avr-libc/user-manual/interrupt_8h_source.html
@@ -13,3 +18,10 @@
 #define ISR(vector, ...)            \
      extern "C" void vector (void)  __VA_ARGS__; \
      void vector (void)
+
+// Possible arguments to the ISR macro
+#define ISR_BLOCK
+#define ISR_NOBLOCK
+#define ISR_NAKED
+#define ISR_ALIASOF(v)
+
